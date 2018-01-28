1b7867387de5c3804785e6bb34b19ff7e1c26728
Tyler <tylersuehr@gmail.com>
2018-01-27 22:44:04 -0500

    Refactored

--- app/src/main/java/com/tylersuehr/chipexample/MainActivity.java
+++ app/src/main/java/com/tylersuehr/chipexample/MainActivity.java
@@ -1,9 +1,13 @@
 package com.tylersuehr.chipexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.tylersuehr.chipexample.testing.ChipsInputTestActivity;
 import com.tylersuehr.chips.ChipsInputLayout;
 
 import java.util.List;
@@ -33,6 +37,20 @@ public class MainActivity extends ContactLoadingActivity
 
         // Load the current user's contact information
         loadContactsWithRuntimePermission();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_test) {
+            startActivity(new Intent(this, ChipsInputTestActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
